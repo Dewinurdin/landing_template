89e1750dc59e2a6e1d9a8989f823ae9f0de913dd
Dewinurdin <dewinurdin@gmail.com>
2020-09-21 12:57:19 -0500

    initial commit

---
+++ src/components/Showcase.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Showcase = () => {
+  return <div className="showcase">
+    <h2>Discover an Adventure</h2>
+    <h1>Eat good Food</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita, eius! 
+        Beatae qui exercitationem earum soluta. 
+    </p>
+    <div className="showcase__buttons">
+      <button className="showcase__btn-services">SERVICES</button>
+      <button className="showcase__btn-about">ABOUT US</button>
+    </div>
+  </div>;
+};
+
+export default Showcase;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import Navbar from './components/Navbar';
+import Showcase from './components/Showcase';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className="main">
+        <Showcase />
+      
+      </div>
+    </div>
+  )
+}
+
+ReactDOM.render(<App/>, document.getElementById("root"))
